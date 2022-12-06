695ec5c017c2f75d9c4dea9a673b4aaa9395ba0c
So Yeon Kim <47154546+72Soyeon@users.noreply.github.com>
2022-12-05 21:07:39 -0500

    HTML+CSS
    -Card update
    -Footer update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(window).scroll(function() {
         $('.scroll').css({
         opacity: function() {
             var elementHeight = $(this).height(),
-            opacity = ((elementHeight - scrollTop) / elementHeight);
+            opacity = ((elementHeight - scrollTop) / (elementHeight*0.5));
             return opacity;
         }
     });
