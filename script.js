05fba0570178706bb95bf70da598670507b8b734
So Yeon Kim <47154546+72Soyeon@users.noreply.github.com>
2022-12-07 01:05:45 -0500

    weather API
    -fetch weather API

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Scroll Animation
 $(window).scroll(function() {
     var scrollTop = $(this).scrollTop();
 
@@ -9,3 +10,18 @@ $(window).scroll(function() {
         }
     });
 });
+
+
+// Weather API
+function weatherBalloon(cityID) {
+    var key = `46319d2aaad3e98dea92fc2f90542066`;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?id=`+cityID+`&appid=`+key)  
+    .then(response=>response.json()
+    .then(data => {
+      console.log(data);
+    }));
+  }
+
+  window.onload = function() {
+    weatherBalloon(6167865);
+  }
