6eaba5ca8902b46b39eec1e08c6cfe3069796099
So Yeon Kim <47154546+72Soyeon@users.noreply.github.com>
2022-12-07 15:42:51 -0500

    Footer change
    About Creators

--- script.js
+++ script.js
@@ -18,39 +18,23 @@ const weather = document.getElementById("weather");
 const city = document.getElementById("location");
 const api_key = `46319d2aaad3e98dea92fc2f90542066`;
 
-
-
-// function weatherCaller(long, lat) {
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?id=`+cityID+`&appid=`+key)  
-//     .then(response=>response.json()
-//     .then(data => {
-//       console.log(data);
-//       console.log(data.weather[0].main);
-//       console.log(data.name);
-//     }));
-//   }
-
-//   window.onload = function() {
-//     weatherCaller(6167865);
-//   }
-
-  function onGeoProper(position){
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
-    fetch(url)
+function onGeoProper(position){
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      city.innerHTML = data.name;
+      city.innerHTML = `${data.name},`;
       weather.innerHTML = `${data.weather[0].main}, ${Math.round(data.main.temp)}°C`;
-    });
+  });
 }
 
 function onGeoError() {
   console.log("No information Found");
-  city.innerHTML = 'Finding the location...'
+  city.innerHTML = 'Oops we are lost!'
   weather.innterHTML = '';
 }
 
