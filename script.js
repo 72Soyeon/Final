920431f6dcdb72b730d31023255bfc3b0a4e0717
So Yeon Kim <47154546+72Soyeon@users.noreply.github.com>
2022-12-12 00:08:23 -0500

    Icon Change
    Weather API Icon Changed

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ $(window).scroll(function() {
 
 
 // Weather API
-
 const weather = document.getElementById("weather");
 const city = document.getElementById("location");
 const api_key = `46319d2aaad3e98dea92fc2f90542066`;
 const tempIcon = document.getElementById("weather-emoji");
 
 function onGeoProper(position){
+<<<<<<< Updated upstream
 const lat = position.coords.latitude;
 const lng = position.coords.longitude;
 //console.log("You live in", lat, lng);
@@ -55,6 +55,43 @@ fetch(url)
       tempIcon.src = './Assets/Images/cloud.svg'  
     }
 });
+=======
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  //console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log (data);
+      city.innerHTML = `${data.name},`;
+      weather.innerHTML = `${data.weather[0].main}, ${Math.round(data.main.temp)}°C`;
+      let id = data.weather[0].main;
+      console.log(id);
+      if(id == "Thunderstorm"){
+        tempIcon.src = './Assets/Images/thunderstorm.svg'
+      } 
+      else if(id == "Drizzle"){
+        tempIcon.src = './Assets/Images/drizzle.svg'  
+      }
+      else if(id == "Rain"){
+        tempIcon.src = './Assets/Images/rain.svg'  
+      }
+      else if(id == "Snow"){
+        tempIcon.src = './Assets/Images/snow.svg'  
+      }
+      else if(id == "Clear"){
+        tempIcon.src = './Assets/Images/sun.svg'  
+      }
+      else if(id == "Clouds"){
+        tempIcon.src = './Assets/Images/cloud.svg'  
+      }
+      else{
+        tempIcon.src = './Assets/Images/atmosphere.svg'  
+      }
+  });
+>>>>>>> Stashed changes
 }
 
 function onGeoError() {
